b2c796f0ff28c49842f3b768b3633017587149e1
standagh <standa31415@gmail.com>
2020-04-06 01:06:23 +0200

    colored output for detailed report - categories

--- timelog_sum.py
+++ timelog_sum.py
@@ -9,6 +9,16 @@ log = logging.getLogger(__name__)
 
 TAGCHAR = "@"
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class SumTimeLog:
     
     CATSEPARATOR = "_"
@@ -97,7 +107,7 @@ class SumTimeLog:
         for cat_combined in sorted(self.cats.keys()):
             totalminutes = 0
             
-            outputstr = "START of output for CATEGORY  %s" % cat_combined
+            outputstr = "START of output for CATEGORY  %s%s%s" % (bcolors.OKGREEN, cat_combined, bcolors.ENDC)
             print("\n"+outputstr+"\n"+"-"*len(outputstr))
             
             totalminperday = 0
